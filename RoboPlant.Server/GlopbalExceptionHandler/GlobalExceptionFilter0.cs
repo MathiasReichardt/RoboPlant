cb300d91d7ecb15494900d411366de3426cfee58
Mathias Reichardt <mathias.reichardt@posteo.de>
2019-01-13 00:21:16 +0100

    Basic MVC setup: - UseMvc - Handle unknown routes - Modify json formatting so no null/default values are serialized - Add HypermediaExtensions - Add GlobalExceptionFilter

---
+++ RoboPlant.Server/GlopbalExceptionHandler/GlobalExceptionFilter0.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using RoboPlant.Server.REST.Problems;
+using WebApi.HypermediaExtensions.ErrorHandling;
+using WebApi.HypermediaExtensions.Exceptions;
+
+namespace RoboPlant.Server.GlopbalExceptionHandler
+{
+    public class GlobalExceptionFilter : IExceptionFilter, IDisposable
+    {
+        private readonly ILogger logger;
+
+        public GlobalExceptionFilter(ILoggerFactory logger)
+        {
+            if (logger != null)
+            {
+                this.logger = logger.CreateLogger("Global Exception Filter");
+            }
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            logger?.LogError("GlobalExceptionFilter", context.Exception);
+
+            ExceptionProblemJson exceptionProblemJson;
+            switch (context.Exception)
+            {
+                case HypermediaFormatterException hypermediaFormatterException:
+                    exceptionProblemJson = ProblemFactory.Exception(hypermediaFormatterException);
+                    break;
+                case HypermediaException hypermediaException:
+                    exceptionProblemJson = ProblemFactory.Exception(hypermediaException);
+                    break;
+                
+                default:
+                    exceptionProblemJson = ProblemFactory.Generic(context.Exception);
+                    break;
+            }
+            
+            CreateResultObject(context, exceptionProblemJson);
+        }
+
+        private static void CreateResultObject(ExceptionContext context, ExceptionProblemJson response)
+        {
+            context.Result = new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode,
+                DeclaredType = response.GetType()
+            };
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
--- RoboPlant.Server/Startup.cs
+++ RoboPlant.Server/Startup.cs
@@ -1,16 +1,43 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Buffers;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using RoboPlant.Server.REST.Problems;
+using WebApi.HypermediaExtensions.WebApi.ExtensionMethods;
+using Bluehands.Hypermedia.MediaTypes;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using RoboPlant.Server.GlopbalExceptionHandler;
 
 namespace RoboPlant.Server
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
+       // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // we will use only lower case in URL paths
+            services.AddRouting(options => options.LowercaseUrls = true);
+            var builder = services.AddMvcCore(options =>
+            {
+                options.OutputFormatters.Clear();
+                options.OutputFormatters.Add(new JsonOutputFormatter(
+                    new JsonSerializerSettings
+                    {
+                        NullValueHandling = NullValueHandling.Ignore,
+                        DefaultValueHandling = DefaultValueHandling.Ignore
+                    }, ArrayPool<char>.Shared));
+
+                // Initializes and adds the Hypermedia Extensions
+                options.AddHypermediaExtensions();
+            });
+            builder.AddMvcOptions(o => { o.Filters.Add(new GlobalExceptionFilter(null)); });
+
+            // Required by Hypermedia Extensions
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -21,9 +48,15 @@ namespace RoboPlant.Server
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseMvc();
+
+            // unknown route
+            app.Run(async context =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var problem = ProblemFactory.NotFound();
+                context.Response.ContentType = DefaultMediaTypes.ProblemJson;
+                context.Response.StatusCode = problem.StatusCode;
+                await context.Response.WriteAsync(JsonConvert.SerializeObject(problem));
             });
         }
     }
