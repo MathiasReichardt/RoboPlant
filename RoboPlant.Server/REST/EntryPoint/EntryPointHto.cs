de4a03e7e499e12c2b385e0e2f6235c78d66d46b
Mathias Reichardt <mathias.reichardt@posteo.de>
2019-01-13 00:27:06 +0100

    Add description and class to EntryPointHto

--- RoboPlant.Server/REST/EntryPoint/EntryPointHto.cs
+++ RoboPlant.Server/REST/EntryPoint/EntryPointHto.cs
@@ -1,7 +1,9 @@
 ﻿using WebApi.HypermediaExtensions.Hypermedia;
+using WebApi.HypermediaExtensions.Hypermedia.Attributes;
 
 namespace RoboPlant.Server.REST.EntryPoint
 {
+    [HypermediaObject(Title = "Entry to the RoboPlant REST API", Classes = new[] { "EntryPoint" })]
     public class EntryPointHto : HypermediaObject
     {
     }
