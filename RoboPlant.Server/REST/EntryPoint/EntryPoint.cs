3c46013ccdb7b0e75e924dd821f4a1a22228ac35
Mathias Reichardt <mathias.reichardt@posteo.de>
2019-01-29 23:14:31 +0100

    Remove comment

--- RoboPlant.Server/REST/EntryPoint/EntryPoint.cs
+++ RoboPlant.Server/REST/EntryPoint/EntryPoint.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApi.HypermediaExtensions.WebApi.AttributedRoutes;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace RoboPlant.Server.REST.EntryPoint
 {
     [Route("api/[controller]")]
