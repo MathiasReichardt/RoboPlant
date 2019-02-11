8d6ed505974103d9b9f32580a19f4cc4c414a8be
Mathias Reichardt <mathias.reichardt@posteo.de>
2019-02-11 18:13:07 +0100

    Add in memory persistence implementation project

---
+++ RoboPlant.InMemoryPersistence/ProductionLineRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RoboPlant.Application.Persistence;
+using RoboPlant.Domain.ProductionLine;
+
+namespace RoboPlant.InMemoryPersistence
+{
+    public class ProductionLineRepository : IProductionLineRepository
+    {
+        public Task<ICollection<ProductionLine>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductionLine> GetById()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
