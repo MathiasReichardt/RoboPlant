759ffea7b41c570691d545ce344e374e4acb46e0
Mathias Reichardt <mathias.reichardt@posteo.de>
2019-05-20 19:55:45 +0200

    Make ShutDownProductionLine async

--- Application/Production/ProductionLineCommandHandler.cs
+++ Application/Production/ProductionLineCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Optional;
 using RoboPlant.Application.Persistence;
 using RoboPlant.Application.Persistence.Results;
 using RoboPlant.Application.Production.Results;
@@ -27,32 +28,42 @@ namespace RoboPlant.Application.Production
         {
             var getProductionLineResult = await this.productionLineRepository.GetById(new ProductionLineId(productionLineId));
 
-            var result = getProductionLineResult.Match<ShutDownProductionLineResult>(
-                success => ExecuteAction(success.Result),
-                notFound => new ShutDownProductionLineResult.NotFound(),
-                notReachablereachable => new ShutDownProductionLineResult.NotReachable(),
-                error => new ShutDownProductionLineResult.Error(error.Exception));
+            var result = await getProductionLineResult.Match<Task<ShutDownProductionLineResult>>(
+                async success => await ExecuteAction(success.Result),
+                async notFound => await Task.FromResult<ShutDownProductionLineResult>(new ShutDownProductionLineResult.NotFound()),
+                async notReachablereachable => await Task.FromResult<ShutDownProductionLineResult>(new ShutDownProductionLineResult.NotReachable()),
+                async error => await Task.FromResult<ShutDownProductionLineResult>(new ShutDownProductionLineResult.Error(error.Exception)));
 
             return result;
         }
 
-        private ShutDownProductionLineResult ExecuteAction(ProductionLine productionLine)
+        private async Task<ShutDownProductionLineResult> ExecuteAction(ProductionLine productionLine)
         {
-            var result = productionLine.ShutDownForMaintenance.Match<ShutDownProductionLineResult>(
-                action => action.Invoke().Match<ShutDownProductionLineResult>(
-                        exception => new ShutDownProductionLineResult.Error(exception),
-                        () =>
-                        {
-                            var addResult = this.productionLineRepository.Add(productionLine).Result; // TODO make proper async
-                            return addResult.Match<ShutDownProductionLineResult>(
-                                success => new ShutDownProductionLineResult.Success(),
-                                notReachable => new ShutDownProductionLineResult.NotReachable(),
-                                error => new ShutDownProductionLineResult.Error(error.Exception)
-                            );
-                        }),
-                () => new ShutDownProductionLineResult.NotAvailable());
+            var result = await productionLine.ShutDownForMaintenance.Match<Task<ShutDownProductionLineResult>>(
+                async action => await InvokeAction(action, productionLine),
+                async () => await Task.FromResult<ShutDownProductionLineResult>(new ShutDownProductionLineResult.NotAvailable()));
 
             return result;
+        }
+        
+        private Task<ShutDownProductionLineResult> InvokeAction(Func<Option<Exception>> action,
+            ProductionLine productionLine)
+        {
+            return action.Invoke().Match<Task<ShutDownProductionLineResult>>(
+                some: async exception =>
+                {
+                    return new ShutDownProductionLineResult.Error(exception);
+                }, 
+                none: async () =>
+                {
+                    var addResult = await this.productionLineRepository.Add(productionLine); // TODO make proper async
+                    var shutDownProductionLineResult = addResult.Match<ShutDownProductionLineResult>(
+                        success => new ShutDownProductionLineResult.Success(),
+                        notReachable => new ShutDownProductionLineResult.NotReachable(),
+                        error => new ShutDownProductionLineResult.Error(error.Exception)
+                    );
+                    return shutDownProductionLineResult;
+                });
         }
     }
 }
