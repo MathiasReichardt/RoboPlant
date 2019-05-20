f94d49d1b5a4c46b7504a6ee6b75843210811d41
Mathias Reichardt <mathias.reichardt@posteo.de>
2019-05-20 22:53:46 +0200

    Add default Result object

---
+++ Util/PatternMatching/Result.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading.Tasks;
+using Util.PatternMatching;
+
+namespace RoboPlant.Util.PatternMatching
+{
+    public abstract class Result<TSuccess, TError>
+    {
+        public void Match(
+            Action<Success> success,
+            Action<Failure> failure)
+            => this.TypeMatch(success, failure);
+
+        public TMatchResult Match<TMatchResult>(
+            Func<Success, TMatchResult> success,
+            Func<Failure, TMatchResult> failure)
+            => this.TypeMatch(success, failure);
+
+        public Task<TMatchResult> Match<TMatchResult>(
+            Func<Success, Task<TMatchResult>> success,
+            Func<Failure, Task<TMatchResult>> failure)
+            => this.TypeMatch(success, failure);
+
+
+        public sealed class Success : Result<TSuccess, TError>
+        {
+            public TSuccess Result { get; }
+
+            public Success(TSuccess result)
+            {
+                Result = result;
+            }
+        }
+
+        public sealed class Failure : Result<TSuccess, TError>
+        {
+            public TError Error { get; }
+
+            public Failure(TError error)
+            {
+                Error = error;
+            }
+        }
+    }
+
+    public abstract class Result<TError>
+    {
+        public void Match(
+            Action<Success> success,
+            Action<Failure> failure)
+            => this.TypeMatch(success, failure);
+
+        public TMatchResult Match<TMatchResult>(
+            Func<Success, TMatchResult> success,
+            Func<Failure, TMatchResult> failure)
+            => this.TypeMatch(success, failure);
+
+        public Task<TMatchResult> Match<TMatchResult>(
+            Func<Success, Task<TMatchResult>> success,
+            Func<Failure, Task<TMatchResult>> failure)
+            => this.TypeMatch(success, failure);
+
+
+        public sealed class Success : Result<TError>
+        {
+            public Success()
+            {
+            }
+        }
+
+        public sealed class Failure : Result<TError>
+        {
+            public TError Error { get; }
+
+            public Failure(TError error)
+            {
+                Error = error;
+            }
+        }
+    }
+}
