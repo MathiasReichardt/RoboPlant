e6a4e99c4f7aba3d5a13a719834a7d18f67ecef2
MathiasReichardt <reichardt@bluehands.de>
2020-05-15 11:34:30 +0200

    Update nuget package

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -43,7 +43,6 @@ namespace RoboPlant.Server
                 new HypermediaExtensionsOptions
                 {
                     ReturnDefaultRouteForUnknownHto = true,
-                    LowercaseUrls = true
                 });
 
             // Infrastructure
